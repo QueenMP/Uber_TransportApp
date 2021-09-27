c3060ad27a134c07ad7aacdcee894f4458a8a94e
QueenMP <queenmalagonp@gmail.com>
2021-09-27 17:49:17 -0500

    cosas locas

--- ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioEstaciones.cs
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioEstaciones.cs
@@ -13,9 +13,9 @@ namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
         {
             estaciones= new List<Estaciones>()
             {
-                new Estaciones{id=1,nombre="Calle 26",direccion="Calle 26",coord_x= 300,coord_y= 234,Tipo= "Ruta facil"},
-                new Estaciones{id=2,nombre="Calle 57",direccion="Calle 56 ",coord_x= 789,coord_y= 123,Tipo= "Ruta facil"},
-                new Estaciones{id=3,nombre="Av Jimenez",direccion="Carrera 9",coord_x= 123,coord_y= 123,Tipo= "Ruta Facil"}
+		        new Estaciones{id=1,nombre="Calle 26",direccion="Calle 26",coord_x= 300,coord_y= 234,tipo= "Ruta facil"},
+                new Estaciones{id=2,nombre="Calle 57",direccion="Calle 56 ",coord_x= 789,coord_y= 123,tipo= "Ruta facil"},
+                new Estaciones{id=3,nombre="Av Jimenez",direccion="Carrera 9",coord_x= 123,coord_y= 123,tipo= "Ruta Facil"}
  
             };
         }
