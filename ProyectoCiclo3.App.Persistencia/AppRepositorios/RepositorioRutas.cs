baab86ecb97c03fb07984ff1599e935d5bddd4c8
QueenMP <queenmalagonp@gmail.com>
2021-09-27 20:36:34 -0500

    agrega edit

--- ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioRutas.cs
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioRutas.cs
@@ -13,11 +13,7 @@ namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
         {
             rutas= new List<Rutas>()
             {
-                new Rutas{id=1,origen=6,destino= 2,tiempo_estimado= 8000},
-                new Rutas{id=2,origen=4,destino= 8,tiempo_estimado= 70000},
-                new Rutas{id=3,origen=1,destino= 2,tiempo_estimado= 20000}
                 
- 
             };
         }
  
